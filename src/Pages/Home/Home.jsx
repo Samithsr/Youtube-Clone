0a6a74f433860a6464bc0ce54e359e907270ccf2
Samithsr <samithrgowda@gmail.com>
2025-04-19 13:03:31 +0530

    adding sidebar and navbar menu-icon toggle function

--- src/Pages/Home/Home.jsx
+++ src/Pages/Home/Home.jsx
@@ -1,10 +1,11 @@
 import React from 'react'
 import  './Home.css'
-const Home = () => {
+import Sidebar from './../../Components/Sidebar/Sidebar';
+const Home = ({sidebar}) => {
   return (
-    <div>
-        
-    </div>
+    <>
+        <Sidebar sidebar={sidebar} />
+    </>
   )
 }
 
