e100f39de1bbfe8f648a0babe5a653e94f35ae6d
Samithsr <samithrgowda@gmail.com>
2025-05-06 15:31:50 +0530

    Play Video will completed

--- src/App.jsx
+++ src/App.jsx
@@ -14,7 +14,7 @@ const App = () => {
         <Route path='/' element={<Home sidebar={sidebar} />} />
         <Route path='/video/:categoryId/:videoId' element={<Video/>} />
       </Routes>
-    </div>
+    </div>  
   )
 }
 
